e578314b956fbf82f634562de0664357e9986be6
Krisztian Dobmayer <dobmayer.krisztian@gmail.com>
2018-04-05 23:35:16 +0200

    Problem004: inner iteration should start from i

--- problem004/main.go
+++ problem004/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	var top int
 	for i := 100; i < 1000; i++ {
-		for j := 100; j < 1000; j++ {
+		for j := i; j < 1000; j++ {
 			if n := i * j; isPalindrom(n) && n > top {
 				top = n
 			}
